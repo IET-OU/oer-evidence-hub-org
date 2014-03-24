d09d8b1d2ad4bb5d8d4fdae0878e94210d120390
Nick Freear <nfreear@yahoo.co.uk>
2014-03-24 22:06:56 +0000

    Add "start_time" etc. to *-quiz-response Javascript/JS
    * Plus, JS Lint fixes.

--- wp-juxtalearn-quiz/js/juxtalearn-quiz-response.js
+++ wp-juxtalearn-quiz/js/juxtalearn-quiz-response.js
@@ -5,33 +5,41 @@ https://github.com/wp-plugins/slickquiz/blob/master/php/slickquiz-front.php#L152
 https://github.com/jewlofthelotus/SlickQuiz
 */
 
+/*jslint indent: 2 */
+/*global jQuery:false, window:false, setTimeout:false, console:false, ajax_url:false, log:false */
+
 jQuery(function ($) {
 
   'use strict';
 
   var $qView = $(".entry-content .slickQuizWrapper"),
     action = 'juxtalearn_quiz_scores',
+    start_time,
     JQ = window.juxtalearn_quiz || {};
 
   log("Quiz response: ", $qView);
 
-  $(".X--button.startQuiz", $qView).on("click", function () {
-    alert("Start click");
+  $qView.on("click", ".button.startQuiz", function () {
+    start_time = new Date();
+    log("Start click:", start_time);
   });
 
-  $qView.on("click", ".button.checkAnswer:last", function (e) {
+//Bug ?!
+  $qView.on("click", ".button.checkAnswer:last", function () {
+    var end_time = new Date(),
+      $the_qz = $(this).closest(".slickQuizWrapper"); //$(e.target)..
 
-    //alert("Check click");
     log("Check-answer click: ", $qView);
-
-    var $the_qz = $(this).closest(".slickQuizWrapper"); //$(e.target)..
 
     setTimeout(function () {
       var quiz_id = $the_qz.attr("id").replace("slickQuiz", ""),
-          quiz_name = $(".quizName", $the_qz).text(),
-          responses = [];
+        quiz_name = $(".quizName", $the_qz).text(),
+        user_name = $(".nameLabel input", $the_qz).val(),
+        user_email = $(".emailLabel input", $the_qz).val(),
+        data = {},
+        responses = [];
 
-      $("#slickQuiz" + quiz_id +" .question").each(function (q_num, el) {
+      $(".question", $the_qz).each(function (q_num, el) {
         responses.push({
           is_correct: $(el).hasClass("correctResponse"),
           q_text: $("h3", $(el)).text(),
@@ -39,11 +47,14 @@ jQuery(function ($) {
         });
       });
 
-      var data = {
-        user_id:   '? todo ?',  //Or, user name for not logged in users
-        quiz_id:   quiz_id,
-        quiz_name: quiz_name,
-        responses: responses
+      data = {
+        user_name:  user_name,  //Or, user name for not logged in users
+        user_email: user_email,
+        quiz_id:    quiz_id,
+        quiz_name:  quiz_name,
+        start_time: start_time.toUTCString(), //toISOString() - IE > 8.
+        end_time:   end_time.toUTCString(),
+        responses:  responses
       };
 
       log(">> Quiz responses: ", data, $the_qz);
@@ -70,7 +81,7 @@ window.log = function () {
   log.history = log.history || [];   // store logs to an array for reference
   log.history.push(arguments);
   if (this.console) {
-    console.log( Array.prototype.slice.call(arguments) );
+    console.log(Array.prototype.slice.call(arguments));
   }
 };
 
