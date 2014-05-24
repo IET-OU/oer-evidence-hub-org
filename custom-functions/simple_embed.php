96e49e4ad4e56d5b8af8f4b5f9301d4c79bfc373
Nick Freear <nfreear@yahoo.co.uk>
2014-05-24 09:44:38 +0100

    Bug #3, Add a user-information JS widget to "Simple Embed" WP plugin
    * Help to debug/ test JuxtaLearn Quiz embeds!

--- custom-functions/simple_embed.php
+++ custom-functions/simple_embed.php
@@ -1,11 +1,11 @@
-<?php
+<?php  #if (!defined('DB_NAME')) die('No direct script access allowed');
 /*
-Plugin Name: JuxtaLearn Simple Embed
+Plugin Name: Simple Embed
 Plugin URI:  https://github.com/IET-OU/oer-evidence-hub-org/#Juxtalearn
 Description: Remove the "chrome" from any Wordpress page, making it embeddable '/page?embed=1&comments=0&target=_blank' [JuxtaLearn].
 Author:  Nick Freear
 Author URI:  https://github.com/nfreear
-Version:  1.2
+Version:  1.3
 */
 define('SIMPLE_EMBED_REGISTER_FILE',
   preg_replace('@/Users/[^\/]+/[^\/]+/[^\/]+@', '',    # Mac OS X
@@ -25,11 +25,11 @@ class Simple_Embed {
 
   public function __construct() {
 
-    $this->is_embed = isset($_GET['embed']);
-    if (isset($_GET['comments']) && $_GET['comments'] < 1) {
+    $this->is_embed = $this->_get( 'embed' );
+    if ($this->_get( 'comments' ) < 1) {
       $this->has_comments = FALSE;
     }
-    if (isset($_GET['target']) && preg_match(self::TARGET_RE, $_GET['target'], $m)) {
+    if ($this->_get('target') && preg_match(self::TARGET_RE, $_GET['target'], $m)) {
       $this->target = $m[1];
     }
 
@@ -47,6 +47,9 @@ class Simple_Embed {
     }
   }
 
+
+  /** WP action: body_class.
+  */
   public function body_class( $classes ) {
     if (!$this->has_comments) {
       if (is_array($classes)) {
@@ -68,28 +71,73 @@ class Simple_Embed {
     return $classes;
   }
 
+  /** WP action. */
   public function enqueue_scripts() {
     wp_enqueue_style('simple-embed', plugins_url(
       'css/simple-embed.css', SIMPLE_EMBED_REGISTER_FILE
     ));
   }
 
+  /** WP action. */
   public function head_target() {
-    $title = '_blank' == $this->target ? __('Opens in new window') : '';
+    $title = ('_blank' == $this->target) ? __('Opens in new window') : '';
     ?>
-    <base id=se-b target="<?php echo $this->target ?>" title="<?php echo $title ?>" />
+    <base id="se-bt" target="<?php echo $this->target ?>" title="<?php echo $title ?>" />
   <?php
   }
 
+  /** WP action. */
   public function footer_script() {
+
+    // Display basic user info on button click.
+    $user = wp_get_current_user();
+    $info = 'Logged in as: [unknown]';
+    if ($user instanceof WP_User) {
+      $info = sprintf('Logged in as: %s (role: %s, via: %s)',
+          $user->user_login, $this->get_user_role(), $this->get_user_auth_method());
+
+      if (0 == $user->ID) {
+        $info = __('Not logged in');
+      }
+    }
+
     ?>
     <script> document.documentElement.className += " simple-embed"; </script>
-    <script>
+    <script id="se-scr">
     jQuery(function ($) {
+      // Set link titles when target='_blank'
       $("a:not( [title] )").attr("title", $("base").attr("title"));
+
+      // User info widget.
+      $(".entry-content:first").append(
+        "<button id='se-info-btn' title='Information'></button>"
+      ).on("click", function () {
+        alert(<?php echo json_encode( $info ) ?>);
+      });
     });
     </script>
   <?php
+  }
+
+
+  /** Utilities.
+  */
+  protected function _get( $key, $default = NULL ) {
+    return isset($_GET[ $key ]) ? $_GET[ $key ] : $default;
+  }
+
+  protected function get_user_role() {
+	global $current_user;
+
+	$user_roles = $current_user->roles;
+	$user_role = array_shift($user_roles);
+	return $user_role;
+  }
+
+  protected function get_user_auth_method() {
+    global $current_user;
+    $via = get_user_meta( $current_user->ID, '_jxl_clipit_auth_method', $single = true );
+    return $via ? $via : '- ';
   }
 
 }
