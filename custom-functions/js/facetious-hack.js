8083ba0e2b5189fcf5de095f6f24fcb7326fee01
Nick Freear <nfreear@yahoo.co.uk>
2014-06-19 22:36:37 +0100

    Bug #18, Fix to "facetious-hack" Javascript

--- custom-functions/js/facetious-hack.js
+++ custom-functions/js/facetious-hack.js
@@ -8,22 +8,23 @@ jQuery(function ($) {
   'use strict';
 
   var $form = $(".widget .facetious_form"),
-    subject = ".facetious_juxtalearn_hub_subject",
+    subject = "facetious_juxtalearn_hub_subject",
+    sel_subject = "." + subject,
     postType = ".facetious_post_type",
     hack = ".hack";
 
   $(postType, $form).addClass("original").hide();
-  $(subject, $form).addClass("original").hide();
+  $(sel_subject, $form).addClass("original").hide();
 
   $(".facetious_search", $form).after("<p class='facetious_post_type hack'>");
   $(postType + hack).html($(postType + ".original").html());
 
   // Put 'subject' after post-type?
   $(postType + hack, $form).after("<p class='" + subject + " hack'>");
-  $(subject + hack).html($(subject + ".original").html());
+  $(sel_subject + hack).html($(sel_subject + ".original").html());
 
   // Remove the duplicate field "name".
   $(postType + ".original").empty();
-  $(subject + ".original").empty();
+  $(sel_subject + ".original").empty();
 
 });
